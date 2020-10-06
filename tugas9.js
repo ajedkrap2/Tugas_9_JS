50ba6247e759d97b93e7512ea1944b2bb79061d9
ajedkrap <mr.thedja@gmail.com>
2020-10-07 00:19:36 +0700

    submit

---
+++ tugas9.js
@@ -0,0 +1,13 @@
+function tugas9(){
+  var mahasiswa = {
+    nama     : "Joni joni yes papa",
+    umur     : 22,
+    jurusan  : "Kedokteran Mesin",
+    semester : 9,
+  }
+  for (var x in mahasiswa) {
+    console.log(mahasiswa[x]);
+  }
+}
+
+tugas9();
